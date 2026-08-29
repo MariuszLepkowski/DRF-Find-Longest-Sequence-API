57c270ef6a8130b886a5cac339d9cff5e1029ccd
Mariusz <mariusz.lepkowski23@gmail.com>
2026-08-29 12:04:06 +0200

    refactor: simplify API to GET-only endpoint

--- core/views.py
+++ core/views.py
@@ -1,41 +1,28 @@
 from rest_framework import status
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from core.clients import ExternalAPIClient, ExternalAPIError
-from core.serializers import (
-    SequenceInputSerializer,
-    SequenceOutputSerializer,
-)
+from core.serializers import SequenceOutputSerializer
 from core.services import find_longest_sequence
 
 
 class ProcessDataAPIView(APIView):
-    serializer_class = SequenceInputSerializer
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
-        raw_sequence = serializer.validated_data["raw_sequence"]
-
-        return self._build_response(raw_sequence)
-
-    def get(self, request, *args, **kwargs):
-        client = ExternalAPIClient()
-
+    def get(
+        self,
+        request: Request,
+        *args,
+        **kwargs,
+    ) -> Response:
         try:
-            raw_sequence = client.get_data()
+            raw_sequence = ExternalAPIClient.get_data()
         except ExternalAPIError:
             return Response(
                 {"error": "Failed to retrieve data from external API."},
                 status=status.HTTP_502_BAD_GATEWAY,
             )
 
-        return self._build_response(raw_sequence)
-
-    @staticmethod
-    def _build_response(raw_sequence: str) -> Response:
         result = find_longest_sequence(raw_sequence)
 
         serializer = SequenceOutputSerializer(
