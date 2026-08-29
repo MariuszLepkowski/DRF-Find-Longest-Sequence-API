e1304134a7b0193a4e9b361d656f254a994ec765
Mariusz <mariusz.lepkowski23@gmail.com>
2026-08-29 12:06:07 +0200

    tests: remove redundant tests connected to POST

--- core/tests.py
+++ core/tests.py
@@ -29,27 +29,6 @@ def api_client():
 
 
 class TestProcessDataAPI:
-    def test_post_success(self, api_client):
-        response = api_client.post(
-            "/api/process/",
-            data={"raw_sequence": "111999922"},
-            format="json",
-        )
-
-        assert response.status_code == 200
-        assert response.data["longest_sequence"] == "9999"
-        assert response.data["length"] == 4
-
-    def test_post_invalid_data_returns_400(self, api_client):
-        response = api_client.post(
-            "/api/process/",
-            data={"raw_sequence": "   "},
-            format="json",
-        )
-
-        assert response.status_code == 400
-        assert "raw_sequence" in response.data
-
     def test_get_external_api_success(self, api_client):
         with patch("core.views.ExternalAPIClient.get_data") as mock_get_data:
             mock_get_data.return_value = "7777722"
